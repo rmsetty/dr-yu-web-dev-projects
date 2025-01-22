acb1e1cd00c531137627348ec01b541f0a033884
rmsetty <rajiv.dfw@gmail.com>
2025-01-22 08:02:52 -0600

    Finished Section 12's Concat.js

---
+++ sections/beginner_JS_12/concat.js
@@ -0,0 +1,5 @@
+var message = "Hello";
+var name = "Rajiv";
+
+//Write your code below to concatenate both into an alert
+alert(message + " there, " + name);
---
+++ sections/beginner_JS_12/jsVarSwap.js
@@ -0,0 +1,16 @@
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 8 - 10:
+
+var c = a;
+a = b;
+b = c;
+    
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
