729dab1a55ede3065bf9686830cf36a8c5467fd9
rmsetty <rajiv.dfw@gmail.com>
2025-01-27 23:39:45 -0600

    Updated Drum Kit.js

--- sections/advanced_JS_18/Drum Kit Starting Files/index.js
+++ sections/advanced_JS_18/Drum Kit Starting Files/index.js
@@ -57,41 +57,95 @@
 // });
 
 // ------------------------ VERSION 3 ----------------------------------
-document.addEventListener("keydown", function (event) {
-    var keyPressed = event.key;
+// document.addEventListener("keydown", function (event) {
+//     var keyPressed = event.key;
 
-    switch(keyPressed)
-    {
-        case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
-          case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-          case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
-          case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-          case "j":
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-          case "k":
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-          case "l":
-            var kickBass = new Audio("sounds/kick-bass.mp3");
-            kickBass.play();
-            break;
-          default:
-            console.log(keyPressed); // Logs the key if it's not in the specified cases
-    }
+//     switch(keyPressed)
+//     {
+//         case "w":
+//             var tom1 = new Audio("sounds/tom-1.mp3");
+//             tom1.play();
+//             break;
+//           case "a":
+//             var tom2 = new Audio("sounds/tom-2.mp3");
+//             tom2.play();
+//             break;
+//           case "s":
+//             var tom3 = new Audio("sounds/tom-3.mp3");
+//             tom3.play();
+//             break;
+//           case "d":
+//             var tom4 = new Audio("sounds/tom-4.mp3");
+//             tom4.play();
+//             break;
+//           case "j":
+//             var snare = new Audio("sounds/snare.mp3");
+//             snare.play();
+//             break;
+//           case "k":
+//             var crash = new Audio("sounds/crash.mp3");
+//             crash.play();
+//             break;
+//           case "l":
+//             var kickBass = new Audio("sounds/kick-bass.mp3");
+//             kickBass.play();
+//             break;
+//           default:
+//             console.log(keyPressed); // Logs the key if it's not in the specified cases
+//     }
+// });
+
+//------------------------ Combining All Versions ----------------------------------
+function makeNoise (key)
+{
+  switch (key)
+  {
+    case 'w':
+      playSound('tom1');
+      break;
+    case 'a':
+      playSound('tom2');
+      break;
+    case 's':
+      playSound('tom3');
+      break;
+    case 'd':
+      playSound('tom4');
+      break;
+    case 'j':
+      playSound('snare');
+      break;
+    case 'k':
+      playSound('crash');
+      break;
+    case 'l':
+      playSound('kick');
+      break;
+    default:
+      console.log('No sound for this key');
+  }
+}
+
+function playSound(key)
+{
+  const sound = new Audio(`sounds/${drum}.mp3`);
+  sound.play();
+}
+
+document.querySelectorAll('.drum').forEach(button => {
+  button.addEventListener("click", function () {
+    var buttonKey = this.innerHTML.toLowerCase();
+    makeSound(buttonKey);
+  });
 });
+
+document.addEventListener("keydown", function () {
+  var key = event.key.toLowerCase();
+  makeSound(key);
+})
+
+document.addEventListener("click", function () {
+  var key = event.target.innerHTML.toLowerCase();
+  makeSound(key);
+})
 
--- sections/advanced_JS_18/jsObjects/houseKeeper.js
+++ sections/advanced_JS_18/jsObjects/houseKeeper.js
@@ -2,6 +2,10 @@ function houseKeeper(yearsOfExperience, name, cleaningRepertoire){
     this.yearsOfExperience = yearsOfExperience;
     this.name = name;
     this.cleaningRepertoire = cleaningRepertoire;
+    this.clean = function (){
+      alert("I can clean");  
+    };
 }
 
-var auntie = new houseKeeper(12, "Jane", ["bathroom", "lobby", "bedroom"])
+var auntie = new houseKeeper(12, "Jane", ["bathroom", "lobby", "bedroom"]);
+auntie.clean();
